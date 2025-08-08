3b94cb8159680b0cb37b884b2721070f1f94fa6a
Joe Decker <blackeclipsevii@users.noreply.github.com>
2025-08-08 16:10:29 -0400

    add tests

--- server/test/Unit.test.js
+++ server/test/Unit.test.js
@@ -18,7 +18,7 @@ test('Optional wargear', () => {
     let unit = army.units['e161-b68c-7d8c-9513'];
     expect(unit.name).toEqual('Chaos Chariot');
     expect(unit).toBeTruthy();
-    expect(unit.models[0].weapons.length).toEqual(2);
+    expect(unit.models[0].weapons.selections.length).toEqual(2);
     expect(unit.models[0].optionSets.length).toEqual(1);
 
     unit = army.units['22ef-99c8-eb17-973f'];
@@ -35,5 +35,49 @@ test('Optional wargear', () => {
     expect(Object.getOwnPropertyNames(unit.models[0].optionSets[0].options)).toEqual([
        "Skystreak Bow",
        "Sunfire Throwers",
+    ]);
+});
+
+test('Multiple model types', () =>{
+    const aos = getAos();
+    let army = aos.getArmy('Cities of Sigmar');
+    expect(army).toBeTruthy();
+    let unit = army.units['9e34-2f0c-69ff-e722'];
+    expect(unit.name).toEqual('Freeguild Command Corps');
+    expect(unit).toBeTruthy();
+    expect(unit.models.length).toBe(6);
+});
+
+test('Weapon selections', () => {
+    const aos = getAos();
+    let army = aos.getArmy('Kharadron Overlords');
+    expect(army).toBeTruthy();
+    let unit = army.units['4c3c-1a00-2c49-87ce'];
+    expect(unit.name).toEqual('Endrinriggers');
+    expect(unit).toBeTruthy();
+    let model = unit.models[0];
+    expect(model.weapons.selections.length).toEqual(2);
+    expect(model.weapons.selectionSets.length).toEqual(2);
+    expect(model.weapons.selectionSets[0].options.length).toEqual(3);
+    let names = model.weapons.selectionSets[0].options.map(option => option.name);
+    expect(names).toEqual([
+        "Skyrigger Heavy Weapon and Gun Butt",
+        "Rapid-fire Rivet Gun",
+        "Aethermatic Saw"
+    ]);
+    
+    army = aos.getArmy('Seraphon');
+    expect(army).toBeTruthy();
+    unit = army.units['9544-33d8-1d69-2be9'];
+    expect(unit.name).toEqual('Kroxigor');
+    expect(unit).toBeTruthy();
+    model = unit.models[0];
+    expect(model.weapons.selections.length).toEqual(1);
+    expect(model.weapons.selectionSets.length).toEqual(1);
+    expect(model.weapons.selectionSets[0].options.length).toEqual(2);
+    names = model.weapons.selectionSets[0].options.map(option => option.name);
+    expect(names).toEqual([
+        "Moonstone Hammer",
+        "Drakebite Maul",
     ]);
 });
