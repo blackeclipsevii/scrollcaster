ee98c0ce48464921a3abeeb1e0560b08b93ba2e1
Joe Decker <blackeclipsevii@users.noreply.github.com>
2025-08-18 19:13:23 -0400

    offline mode

--- app/lib/RestAPI/tactics.ts
+++ app/lib/RestAPI/tactics.ts
@@ -1,6 +1,7 @@
 import { fetchWithLoadingDisplay } from "./fetchWithLoadingDisplay.js"
 import { endpoint } from "../endpoint.js";
+import BattleTacticCardInterf from "../../shared-lib/BattleTacticCardInterf.js";
 
-export const fetchTactics = async () => {
+export const fetchTactics = async (): Promise<null | BattleTacticCardInterf[]> => {
     return await fetchWithLoadingDisplay(encodeURI(`${endpoint}/tactics`));
 };
--- app/lib/RestAPI/upgrades.ts
+++ app/lib/RestAPI/upgrades.ts
@@ -1,6 +1,7 @@
 import { fetchWithLoadingDisplay } from "./fetchWithLoadingDisplay.js"
 import { endpoint } from "../endpoint.js";
+import { ArmyUpgrades } from "../../shared-lib/ArmyUpgrades.js";
 
-export const fetchUpgrades = async (armyName: string) => {
+export const fetchUpgrades = async (armyName: string): Promise<ArmyUpgrades | null>=> {
     return await fetchWithLoadingDisplay(encodeURI(`${endpoint}/upgrades?army=${armyName}`));
 };
