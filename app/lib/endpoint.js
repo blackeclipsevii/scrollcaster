99816a5d8b5d48794b8c711b08beabffe93d05c4
Joe Decker <blackeclipsevii@users.noreply.github.com>
2025-08-22 12:49:18 -0400

    Update endpoint.js

--- app/lib/endpoint.js
+++ app/lib/endpoint.js
@@ -1,3 +1,8 @@
-export const hostname = "http://localhost"//"https://army-thing.fly.dev";
-export const port = 3000;
+const params = new URLSearchParams(window.location.search);
+export let hostname = "https://army-thing.fly.dev";
+export let port = null;
+if (params.get('localhost')) {
+    hostname = 'http://localhost';
+    port = 3000;
+}
 export const endpoint = port ? `${hostname}:${port}` : hostname;
