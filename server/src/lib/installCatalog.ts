3abcb6053c76df348ac3556b177bd4b5015b3f1f
Joe Decker <blackeclipsevii@users.noreply.github.com>
2025-08-23 20:52:29 -0400

    lazy convert to ts

--- server/src/lib/installCatalog.ts
+++ server/src/lib/installCatalog.ts
@@ -2,11 +2,14 @@ import fs from 'fs';
 import path from 'path';
 import axios from 'axios';
 import extract from 'extract-zip';
+
 const repoOwner = 'BSData';
 const repoName = 'age-of-sigmar-4th';
 const repoUrl = `https://github.com/${repoOwner}/${repoName}`;
 const extractPath = path.resolve('./data');
-export const deleteDataFolder = () => {
+
+// Delete the data folder
+export const deleteDataFolder = ():void => {
     try {
         fs.rmSync(extractPath, { recursive: true, force: true });
         console.log('Folder deleted successfully.');
@@ -15,57 +18,70 @@ export const deleteDataFolder = () => {
         console.error('Error deleting folder:', err);
     }
 };
-const getLatestCommitId = async () => {
-    let commitId = 'unknown';
+
+// Get the latest master commit id
+const getLatestCommitId = async (): Promise<string> => {
+    let commitId: string | undefined = 'unknown';
     await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/commits`)
-        .then(response => {
+    .then(response => {
         if (!response.ok) {
             throw new Error(`GitHub API error: ${response.status}`);
         }
         return response.json();
     })
-        .then(commits => {
+    .then((_commits: unknown) => {
+        const commits = _commits as {sha: string | undefined}[]; 
         commitId = commits[0]?.sha;
     })
-        .catch(error => {
+    .catch(error => {
         console.error("Error fetching commit data:", error);
     });
     return commitId;
 };
-const getCommitIdFromTag = async (tag) => {
+
+// Get the commit id based on a tag
+const getCommitIdFromTag = async (tag: string): Promise<string> => {
     let commitId = 'unknown';
     const uri = encodeURI(`https://api.github.com/repos/${repoOwner}/${repoName}/git/ref/tags/${tag}`);
     console.log(uri);
     await fetch(uri)
-        .then(res => res.json())
-        .then(refData => {
+    .then(res => res.json())
+    .then(_refData => {
+        const refData = _refData as {url: string};
         const tagUrl = refData.url;
         // Fetch the tag object to get the commit SHA
         return fetch(tagUrl);
     })
-        .then(res => res.json())
-        .then(tagData => {
-        commitId = tagData.object.sha;
+    .then(res => res.json())
+    .then(_tagData => {
+        const tagData = _tagData as {[name: string]: unknown};
+        commitId = (tagData.object as {[name: string]: string}).sha;
         console.log(`Commit ID for tag ${tag}:`, commitId);
     })
-        .catch(err => console.error('Error fetching tag info:', err));
+    .catch(err => console.error('Error fetching tag info:', err));
     return commitId;
 };
-export default async function installCatalog(tag, commitSha) {
+
+export default async function installCatalog(tag ?: string, commitSha ?: string): Promise<string> {
     let zipUrl;
     let commitId;
+
+    // sha option
     if (commitSha) {
         zipUrl = encodeURI(`${repoUrl}/archive/${commitSha}.zip`);
         commitId = commitSha;
     }
+    // tag option
     else if (tag) {
         zipUrl = encodeURI(`${repoUrl}/archive/refs/tags/${tag}.zip`);
         commitId = await getCommitIdFromTag(tag);
     }
+    // default (latest master)
     else {
         zipUrl = encodeURI(`${repoUrl}/archive/refs/heads/main.zip`);
         commitId = await getLatestCommitId();
     }
+
     try {
         // Download the ZIP file
         const response = await axios({
@@ -77,7 +93,7 @@ export default async function installCatalog(tag, commitSha) {
         const writer = fs.createWriteStream(zipPath);
         response.data.pipe(writer);
         await new Promise((resolve, reject) => {
-            writer.on('finish', resolve);
+            writer.on('finish', resolve as (() => void));
             writer.on('error', reject);
         });
         console.log('Download complete.');
@@ -88,7 +104,7 @@ export default async function installCatalog(tag, commitSha) {
         fs.unlinkSync(zipPath);
     }
     catch (error) {
-        console.error('Error:', error.message);
+        console.error('Error:', (error as Error).message);
     }
     return commitId;
 }
